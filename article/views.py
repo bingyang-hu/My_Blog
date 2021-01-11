7d72e998c75225eafda4948952debbe7eb4569fa
bingyang-hu <zoey4y@gmail.com>
2021-01-10 23:37:32 -0800

    Update views.py

--- article/views.py
+++ article/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import ArticlePost
+
 
 def article_list(request):
-    return HttpResponse("Hello World!")
+
+    articles = ArticlePost.objects.all()
+
+    context={'articles':articles}
+
+    return render(request,'article/list.html',context)
